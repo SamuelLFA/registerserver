0ee9ad1c7750bdf81a2486e0f2e2b63a2aec2082
SamuelLFA <samuellfa3@gmail.com>
2022-06-04 16:23:44 -0300

    add database connect env var

--- src/database/database.go
+++ src/database/database.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/samuellfa/registerserver/src/models"
 	"gorm.io/driver/postgres"
@@ -14,7 +16,13 @@ var (
 )
 
 func ConnectWithDatabase() {
-	dsn := "host=localhost user=root password=root dbname=register port=5432 sslmode=disable"
+	db_host := os.Getenv("DB_HOST")
+	db_username := os.Getenv("DB_USERNAME")
+	db_password := os.Getenv("DB_PASSWORD")
+	db_port := os.Getenv("DB_PORT")
+	db_name := os.Getenv("DB_NAME")
+	db_ssl := os.Getenv("DB_SSL")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", db_host, db_username, db_password, db_name, db_port, db_ssl)
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
